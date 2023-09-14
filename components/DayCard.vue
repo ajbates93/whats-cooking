9640b9e4aa8505c3c5dde704dec56ac32418402f
Alex Bates <ajbates93@gmail.com>
2023-09-14 10:52:18 +0100

    feat: filterable 'daysToShow' value which will alter the length of the array.

---
+++ components/DayCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+type Props = {
+  dayTitle: string
+}
+const { dayTitle } = defineProps<Props>()
+
+</script>
+
+<template>
+  <div>
+    <div class="title">{{ dayTitle }}</div>
+    <div>Breakfast</div>
+    <div>...</div>
+    <div>Lunch</div>
+    <div>...</div>
+    <div>Dinner</div>
+    <div>...</div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,33 @@
 <script setup lang="ts">
 
+const daysToShow = ref<number>(7)
+
+const calendarDays = computed(() => {
+  const today = new Date()
+  return Array.from({ length: daysToShow.value }, (_, i) => {
+    const day = new Date(today)
+    if (i > 0)
+      day.setDate(today.getDate() + i)
+    return day
+  })
+})
+
 </script>
+
 <template>
   <div>
-
+    <h1>What's Cooking?</h1>
+    <div>
+      <span>Days to show: </span>
+      <select v-model="daysToShow">
+        <option>5</option>
+        <option selected>7</option>
+        <option>10</option>
+        <option>14</option>
+      </select>
+    </div>
+    <div>
+      <DayCard v-for="day in calendarDays" :day-title="day.toLocaleDateString('en-UK', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'})"></DayCard>
+    </div>
   </div>
 </template>
