aa419a5b0ae54bd7c15d88c2ed17ce0f42a2dd66
Alex Bates <ajbates93@gmail.com>
2023-09-15 10:45:17 +0100

    feat: add testing suites

---
+++ pages/index.test.ts
@@ -0,0 +1,15 @@
+import { mount } from '@vue/test-utils'
+import { describe, expect, test } from 'vitest'
+import Homepage from './index.vue'
+
+describe('Homepage', () => {
+  const wrapper = mount(Homepage)
+  test('default number of days', () => {
+    expect(wrapper.vm.$refs.daysToShow).toBe(7)
+  })
+
+  test('select a different number of days', () => {
+    wrapper.vm.$refs.daysToShow.value = 5
+    expect(wrapper.vm.$refs.calendarDays.value.length).toBe(10)
+  })
+})
