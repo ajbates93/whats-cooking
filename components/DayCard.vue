d0d180aa0dbebb5cbb4ac956b91b66a8adccf042
Alex Bates <ajbates93@gmail.com>
2023-09-15 17:27:58 +0100

    feat: add nuxt-icon to display when a meal has been submitted.

--- components/DayCard.vue
+++ components/DayCard.vue
@@ -31,17 +31,29 @@ const addMeal = (mealType: string, mealTitle: string) => {
 
 <template>
   <div class="shadow rounded-lg p-5 bg-white border-2 border-primary border-opacity-30">
-    <div class="title text-xl font-bold mb-2 text-gray-700">{{ dayTitle }}</div>
+    <div class="title text-xl font-bold mb-2 text-gray-700 flex justify-between items-center">
+      {{ dayTitle }}
+      <Icon v-if="day.breakfast && day.lunch && day.dinner" name="carbon:checkbox-checked-filled" class="text-green-500" size="2rem" />
+    </div>
     <div class="my-3">
-      <div class="my-2 font-bold text-primary text-lg">Breakfast</div>
+      <div class="my-2 font-bold text-primary text-lg flex justify-between items-center">
+        Breakfast
+        <Icon v-if="day.breakfast" name="carbon:checkbox-checked-filled" class="text-green-500" size="1.5rem"/>
+      </div>
       <DayMeal :day="day" :meal="day.breakfast ?? null" @addMeal="(mealTitle: string) => addMeal('breakfast', mealTitle)" />
     </div>
     <div class="my-3">
-      <div class="my-2 font-bold text-primary text-lg">Lunch</div>
+      <div class="my-2 font-bold text-primary text-lg flex justify-between items-center">
+        Lunch
+        <Icon v-if="day.lunch" name="carbon:checkbox-checked-filled" class="text-green-500" size="1.5rem"/>
+      </div>
       <DayMeal :day="day" :meal="day.lunch ?? null" @addMeal="(mealTitle: string) => addMeal('lunch', mealTitle)" />
     </div>
     <div class="my-3">
-      <div class="my-2 font-bold text-primary text-lg">Dinner</div>
+      <div class="my-2 font-bold text-primary text-lg flex justify-between items-center">
+        Dinner
+        <Icon v-if="day.dinner" name="carbon:checkbox-checked-filled" class="text-green-500" size="1.5rem"/>
+      </div>
       <DayMeal :day="day" :meal="day.dinner ?? null" @addMeal="(mealTitle: string) => addMeal('dinner', mealTitle)" />
     </div>
   </div>
