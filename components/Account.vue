7df1478d339b784afd5543b3dc67f466f185e675
Alex Bates <ajbates93@gmail.com>
2023-10-30 21:08:11 +0000

    feat: add household composable

--- components/Account.vue
+++ components/Account.vue
@@ -26,8 +26,13 @@
     </form>
     <h2 class="text-gray-600 flex items-center"><span>Your Household</span><Icon class="ml-5 text-primary" name="carbon:home"></Icon></h2>
     <div class="my-5">
-      <div class="text-xl text-gray-600">Add, edit or remove members of your household.</div>
-      
+      <template v-if="household">
+        <div class="text-xl text-gray-600 pb-5">Household name: <span class="underline ml-2">{{ household.name }}</span></div>
+        <div class="text-md bg-gray-400 py-1 px-2 rounded text-white inline-flex items-center"><Icon class="mr-2" size="1.25rem" name="carbon:information"></Icon> Add, edit or remove members of your household.</div>
+      </template>
+      <template v-else>
+        <LayoutActionButton @click="createHousehold('Bates-Lavelle')" class="bg-primary">Create Household</LayoutActionButton> 
+      </template>
     </div>
   </div>
 
@@ -36,15 +41,17 @@
 <script lang="ts" setup>
 
 const { user, updateProfile, fetchProfile, loading } = useAuth()
+const { createHousehold, fetchHouseholdForAdmin, loading: householdLoading } = useHousehold()
 
 const username = ref('')
 const avatar_path = ref('')
 
-const { data } = await fetchProfile()
+const { data: profile } = await fetchProfile()
+const { data: household } = await fetchHouseholdForAdmin()
 
-if (data) {
-  username.value = data.username
-  avatar_path.value = data.avatar_url
+if (profile) {
+  username.value = profile.username
+  avatar_path.value = profile.avatar_url
 }
 
 const handleUpdateProfileSubmit = () => updateProfile(username.value, avatar_path.value);
---
+++ composables/household.ts
@@ -0,0 +1,38 @@
+type Household = {
+}
+
+export const useHousehold = () => {
+  const supabase = useSupabaseClient()
+  const { user } = useAuth()
+  const loading = ref(false)
+
+  const createHousehold = async (name: string) => {
+    try {
+      if (!user.value) return;
+      const household = {
+        created_by: user.value.id,
+        admin_id: user.value.id,
+        name: name
+      }
+      loading.value = true
+      const { error } = await supabase
+        .from('households')
+        .insert(household)
+    } catch (error) {
+      console.error(error)
+    } finally {
+      loading.value = false
+    }
+  } 
+
+  const fetchHouseholdForAdmin = async () => {
+    const { data } = await supabase
+      .from('households')
+      .select('id, name')
+      .eq('admin_id', user.value?.id)
+      .single()
+    return { data } 
+  }
+
+  return { createHousehold, fetchHouseholdForAdmin, loading }
+}
