a647511a513806b334dc0d7dc0d008c4acef357b
Alex Bates <ajbates93@gmail.com>
2023-11-12 21:05:40 +0000

    #8 feat: start adding ui

--- pages/recipe-suggestion/index.vue
+++ pages/recipe-suggestion/index.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>
-    test
+  <div class="flex items-center">
+    <div class="mr-2">Ingredients:</div>
+    <input class="border p-1 mr-2" type="text" v-model="newIngredient" /> 
+    <LayoutActionButton :class="{'bg-primary': newIngredient.length > 0 }">Submit</LayoutActionButton>
   </div>
 </template>
 
 <script lang="ts" setup>
+
+const newIngredient = ref('')
 
 </script>
 
