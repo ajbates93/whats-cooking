0185ba7ef4841b7bbd8d6bed1a2ed9e58c2f2679
Alex Bates <ajbates93@gmail.com>
2023-11-12 20:48:30 +0000

    #8 feat: init page for recipe suggestions

--- components/Header.vue
+++ components/Header.vue
@@ -7,6 +7,9 @@ const { user, signOut, signIn } = useAuth()
     <div class="flex items-center justify-between py-4 max-w-screen-xl xl:max-w-screen-2xl mx-auto">
       <h1 class="text-2xl text-center"><NuxtLink to="/">What's <span class="text-primary dark:text-primary-dark">Cooking</span>?</NuxtLink></h1>
       <div class="flex items-center">
+        <NuxtLink to="/recipe-suggestion">
+          <div class="border rounded-2xl py-1 px-3 border-primary text-primary mx-2 items-center flex"><Icon name="carbon:star" class="mr-1" /> Recipe Suggestions</div> 
+        </NuxtLink>
         <NuxtLink to="/meal-planner">
           <LayoutActionButton class="bg-primary dark:bg-primary-dark mr-1">Meal Planner</LayoutActionButton>
         </NuxtLink>
@@ -15,7 +18,7 @@ const { user, signOut, signIn } = useAuth()
         </NuxtLink>
         <template v-if="user">
           <NuxtLink to="/account">
-            <LayoutActionButton class="bg-primary dark:bg-primary-dark mr-1">Account</LayoutActionButton>
+            <LayoutActionButton class="bg-gray-600 dark:bg-primary-dark mr-1">Account</LayoutActionButton>
           </NuxtLink>
           <LayoutActionButton @click="signOut">Log Out</LayoutActionButton>
         </template>
