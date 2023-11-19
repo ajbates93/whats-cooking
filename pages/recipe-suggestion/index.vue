49001508446184d1d3dcdf1555b61a6917197981
Alex Bates <ajbates93@gmail.com>
2023-11-19 21:11:00 +0000

    feat: working on handle recipe suggestions

--- pages/recipe-suggestion/index.vue
+++ pages/recipe-suggestion/index.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="flex items-center">
-    <div class="mr-2">Ingredients:</div>
-    <input class="border p-1 mr-2" type="text" v-model="newIngredient" /> 
-    <LayoutActionButton :class="{'bg-primary': newIngredient.length > 0 }">Submit</LayoutActionButton>
+  <div>
+    <form class="flex items-center" @submit.prevent="handleNewIngredientSubmit" @input:enter="handleNewIngredientSubmit">
+      <div class="mr-2">
+        Enter Ingredient:
+      </div>
+      <input v-model="newIngredient" class="border p-1 mr-2" type="text" @input:enter="handleNewIngredientSubmit">
+      <button type="submit">
+        <LayoutActionButton
+          :class="{ 'bg-primary': newIngredient.length > 0 }"
+        >
+          Submit
+        </LayoutActionButton>
+      </button>
+    </form>
+    <div class="my-3">
+      {{ allIngredients.join(", ") }}
+    </div>
+    <div class="my-3 inline-block">
+      <LayoutActionButton class="bg-[#a5da4f]">
+        Generate Ideas
+      </LayoutActionButton>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-
+// Ingredients
 const newIngredient = ref('')
+const allIngredients = ref<string[]>([])
 
+// Handle
+function handleNewIngredientSubmit() {
+  if (allIngredients.value.includes(newIngredient.value))
+    return
+  allIngredients.value.push(newIngredient.value)
+  newIngredient.value = ''
+}
 </script>
 
-<style>
-
-</style>
-
+<style></style>
